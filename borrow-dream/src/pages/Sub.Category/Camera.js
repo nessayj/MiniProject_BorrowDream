0edd1cf8c5f0227c3f73faf705beff57ed0174f7
1235rtyu <ch07124@naver.com>
2023-05-21 14:44:10 +0900

    민혁1444

--- borrow-dream/src/pages/Sub.Category/Camera.js
+++ borrow-dream/src/pages/Sub.Category/Camera.js
@@ -1,17 +1,49 @@
 
 import Card from "./data/Card";
 
+import React from "react";
+import styled from "styled-components";
+
+const Button = styled.button`
+font-family:'bitbit';
+margin-left: 160px;
+  width: 7rem;
+  background-color: #135CD2;
+  color: white;
+  height: 3rem;
+  border: none;
+  transition: all .1s ease-in;
+  border-radius: 0.7rem;
+  &:hover {
+    background-color: #a1f7d9;
+    color: #135CD2;
+  }
+  font-size: 1.2rem;
+`;
+const Heading1 = styled.h1`
+  font-size: 2rem;
+  color: #135CD2;
+  margin-bottom: 1rem;
+  font-family:'bitbit';
+  margin-left: 50px;
+`;
 
 
-const Camera = () => {
+const Carrier = () => {
+
+  const goToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
   return (
     <>
-      <h1 >카메라</h1>
+      <Heading1>카메라</Heading1>
       <Card categoryNo={1002} />
-      
+      <div className="goTop">
+        <Button onClick={goToTop}>맨위로</Button>
+      </div>
     </>
   );
 };
 
-
-export default Camera;
+export default Carrier;
