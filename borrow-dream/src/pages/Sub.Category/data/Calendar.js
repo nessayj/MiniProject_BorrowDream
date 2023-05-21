c5c434383e92c8bf8748712ee72b23fff8254800
1235rtyu <ch07124@naver.com>
2023-05-21 21:00:53 +0900

    900

--- borrow-dream/src/pages/Sub.Category/data/Calendar.js
+++ borrow-dream/src/pages/Sub.Category/data/Calendar.js
@@ -1,24 +1,35 @@
 import 'react-date-range/dist/styles.css';
 import 'react-date-range/dist/theme/default.css';
 import { DateRange } from 'react-date-range';
-import React, { Component, useContext } from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 import ko from 'date-fns/locale/ko';
 import styled from 'styled-components';
-import { UserContext } from '../../../context/userInfo';
 
 const CalendarContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   position: relative;
-  top: 37px;
+  top: 97px;
+  
+  .day{
+    position: relative;
+    right: 200px;
+    top: 25px;
+    
+  }
+  .total{
+    position: relative;
+    bottom: 49px;
+    color: orangered;
+  }
 `;
 
 const DateLabel = styled.div`
   font-size: 1.2rem;
   margin-bottom: 0.5rem;
-  margin-left: 200px;
+  margin-left: 200px; 
 `;
 
 class CalendarComponent extends Component {
@@ -38,6 +49,7 @@ class CalendarComponent extends Component {
     const endDate = ranges['selection'].endDate;
     const totalDays = parseInt(Math.ceil((endDate - startDate) / (1000 * 60 * 60 * 24))+1);
     const totalPrice = this.calculatePriceByDate(startDate, endDate);
+
     this.setState({
       startDate,
       endDate,
@@ -55,7 +67,6 @@ class CalendarComponent extends Component {
       });
       console.log('날짜 범위 저장 성공:', response.data);
       window.alert('날짜 범위 저장 성공');
-      
     } catch (error) {
       console.error('날짜 범위 저장 실패:', error);
       window.alert('날짜 범위 저장 실패');
@@ -68,8 +79,9 @@ class CalendarComponent extends Component {
     const totalPrice = productPrice * (days + 1); // 가격 계산에 1을 더해줌
     return totalPrice;
   };
+  
+
   render() {
-    const { startDate, endDate, totalDays, totalPrice} = this.state;
     return (
       <CalendarContainer>
         <DateRange
@@ -79,10 +91,14 @@ class CalendarComponent extends Component {
           ranges={[this.state]}
           locale={ko}
         />
-        <DateLabel>물건 대여: {startDate.toLocaleDateString()}</DateLabel>
-        <DateLabel>물건 반납: {endDate.toLocaleDateString()}</DateLabel>
-        <DateLabel>총 일 수: {totalDays}일</DateLabel>
-        <DateLabel>총 가격: {totalPrice}원</DateLabel>
+        <div className=' day'>
+        <DateLabel>물건 대여: {this.state.startDate.toLocaleDateString()}</DateLabel>
+        <DateLabel>물건 반납: {this.state.endDate.toLocaleDateString()}</DateLabel>
+        </div>
+        <div className='total'>
+        <DateLabel>총 일 수: {this.state.totalDays}일</DateLabel>
+        <DateLabel>총 가격: {this.state.totalPrice}원</DateLabel>
+        </div>
       </CalendarContainer>
     );
   }
