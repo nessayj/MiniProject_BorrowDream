b9c8f1a5918eef92e719d295d8c8ebf68034d65f
1235rtyu <ch07124@naver.com>
2023-05-22 15:10:42 +0900

    0310

--- borrow-dream/src/pages/Sub.Category/Cooking.js
+++ borrow-dream/src/pages/Sub.Category/Cooking.js
@@ -1,14 +1,52 @@
-
+import React from "react";
+import styled from "styled-components";
+import { SiStarship } from "react-icons/si";
 import Card from "./data/Card";
 
+const Button = styled.button`
+font-family:'bitbit';
+margin-left: 160px;
+  width: 7rem;
+  background-color: #135CD2;
+  color: white;
+  height: 3rem;
+  border: none;
+  transition: all .1s ease-in;
+  border-radius: 0.7rem;
+  &:hover {
+    background-color: #a1f7d9;
+    color: #135CD2;
+  }
+  font-size: 1.2rem;
+`;
+
+const Heading1 = styled.h1`
+  font-family: 'bitbit';
+  font-weight: 600;
+  font-size: 2rem;
+  color: #135CD2;
+  margin-bottom: 2rem;
+  margin-left: 11rem;
+  display: flex;
+  align-items: center;
+`;
 
 
 const Cooking = () => {
+  const goToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
   return (
     <>
-      <h1 >취사용품</h1>
+      <Heading1>
+        <SiStarship size="30" color="7F8EEF" />
+        요리도구
+      </Heading1>
       <Card categoryNo={4002} />
-      
+      <div className="goTop">
+        <Button onClick={goToTop}>맨위로</Button>
+      </div>
     </>
   );
 };
