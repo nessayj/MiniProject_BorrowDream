7956ebe598b1f6e59cdc5d138849d18583f3a193
Chosh93 <chosh930911@gmail.com>
2023-05-22 14:45:43 +0900

    202305221445 승혁

--- borrow-dream/src/pages/order/pay.js
+++ borrow-dream/src/pages/order/pay.js
@@ -4,20 +4,20 @@ import { UserContext } from "../../context/userInfo";
 import OrderApi from "../../api/orderApi";
 
 export const Pay = ({ orderList, setOrderList, convertPrice }) => {
-  const context = useContext(UserContext);
-  const { userId } = context;
-      
+  // const context = useContext(UserContext);
+  // const { userId } = context;
+  const Id = window.localStorage.getItem("Id");
   useEffect(() => {
     const cartList = async () => {
       try {
-        const response = await OrderApi.orderListGet(userId);
+        const response = await OrderApi.orderListGet(Id);
         setOrderList(response.data)
       } catch (e) {
         console.log(e);
       }
     }
     cartList();
-  }, [userId])
+  }, [Id])
   return (
     <>
       <OrderInfo orderList={orderList} convertPrice={convertPrice} />
